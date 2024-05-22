7e2bde887d06f10badbaba4164392f4f17162bc3
Jinhyukpark56 <109671126+Jinhyukpark56@users.noreply.github.com>
2024-05-22 21:16:58 +0900

    Create 27160.py

--- boj_algorithm/1026.py
+++ boj_algorithm/1026.py
@@ -6,7 +6,7 @@ b_list = list(map(int,input().split()))
 sorted_a = sorted(a_list, reverse=True)
 sorted_b= sorted(b_list)
 
-s= 0
+s = 0
 
 for i in range(n):
     s += sorted_a[i] * sorted_b[i]
--- boj_algorithm/10825.py
+++ boj_algorithm/10825.py
@@ -1,12 +1,12 @@
 # lambda 함수의 기본 형식 : lsit.sort(key=lambda x:(정렬 대상) or sorted(list.key = lambda x:(정렬 대상)))
 
-n = int(input())
-students = []
+N = int(input()) ; arr = []
 
-for _ in range(n):
-    students.append(input().split())
-    
-students.sort(key = lambda x: (-int(x[1]), int(x[2]), -int([3]), x[0]))
+for _ in range(N):
+    a,b,c,d = list(map(str,input().split()))
+    arr.append( [a, int(b),int(c),int(d)])
 
-for student in students:
-    print(student[0])
+arr.sort(key = lambda x : (-x[1] , x[2],-x[3],x[0]) )
+
+for i in arr :
+    print(i[0])
