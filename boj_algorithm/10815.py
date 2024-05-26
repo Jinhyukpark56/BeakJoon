e58ac9963c1e32b19a1b106fd7fcdd5f18fc9a03
Jinhyukpark56 <109671126+Jinhyukpark56@users.noreply.github.com>
2024-05-26 19:16:51 +0900

    update

--- boj_algorithm/10815.py
+++ boj_algorithm/10815.py
@@ -3,9 +3,14 @@ card = list(map(int, input().split()))
 M = int(input())
 other = list(map(int, input().split()))
 
+dic = {}
 
 for o in other:
-    if o in card:
-        print(1, end=' ')
-    else:
-        print(0, end=' ')
+    dic[o] = 0
+
+for c in card:
+    if c in dic:
+        dic[c] = 1
+
+for d in dic:
+    print(dic[d], end=' ')
