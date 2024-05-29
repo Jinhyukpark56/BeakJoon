3b2e3956b688a9cafe5b55af35baa94a574c03e9
Jinhyukpark56 <109671126+Jinhyukpark56@users.noreply.github.com>
2024-05-29 21:14:48 +0900

    Create 10814.py

---
+++ boj_algorithm/10814.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+user = []
+
+for _ in range(n):
+    age, name = input().split()
+    user.append([int(age),name])
+    
+for i in sorted(user, key=lambda x : x[0]):
+    print(i[0],i[1])
