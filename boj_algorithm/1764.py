ec1d0a99629029f069b034e15a5a6a9656ffac93
Jinhyukpark56 <109671126+Jinhyukpark56@users.noreply.github.com>
2024-06-03 20:55:49 +0900

    Create 2828.py

---
+++ boj_algorithm/1764.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+listen = [input().rstrip() for _ in range(N)]
+see = [input().rstrip() for _ in range(M)]
+
+ans = list(set(listen) & set(see))
+ans.sort()
+print(len(ans))
+for a in ans:
+    print(a)
